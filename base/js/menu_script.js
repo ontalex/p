dfa72427bb5bfecf1e73289d34018f1e131b2eee
alesiobb <loja.bobaev2000@yandex.ru>
2020-08-18 13:55:44 +0300

    Чистка

--- base/js/menu_script.js
+++ base/js/menu_script.js
@@ -1,42 +1,6 @@
-$(function(){
-    $("#buttomMenu1").mouseover(
-        function() {
-            $("#noneMenu1").css("display","block");
-        }
-    )
-});
-$(function(){
-    $("#buttomMenu1").mouseleave(
-        function() {
-            $("#noneMenu1").css("display","none");
-        }
-    )
-});
-$(function(){
-    $("#buttomMenu2").mouseover(
-        function() {
-            $("#noneMenu2").css("display","block");
-        }
-    )
-});
-$(function(){
-    $("#buttomMenu2").mouseleave(
-        function() {
-            $("#noneMenu2").css("display","none");
-        }
-    )
-});
-$(function(){
-    $("#buttomMenu3").mouseover(
-        function() {
-            $("#noneMenu3").css("display","block");
-        }
-    )
-});
-$(function(){
-    $("#buttomMenu3").mouseleave(
-        function() {
-            $("#noneMenu3").css("display","none");
-        }
-    )
-});
+$(function(){$("#buttomMenu1").mouseover(function() {$("#noneMenu1").css("display","block");})});
+$(function(){$("#buttomMenu1").mouseleave(function() {$("#noneMenu1").css("display","none");})});
+$(function(){$("#buttomMenu2").mouseover(function() {$("#noneMenu2").css("display","block");})});
+$(function(){$("#buttomMenu2").mouseleave(function() {$("#noneMenu2").css("display","none");})});
+$(function(){$("#buttomMenu3").mouseover(function() {$("#noneMenu3").css("display","block");})});
+$(function(){$("#buttomMenu3").mouseleave(function() {$("#noneMenu3").css("display","none");})});
