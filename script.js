07674d4528d42ef0bb95920b721d531579c5d3cc
alesiobb <loja.bobaev2000@yandex.ru>
2021-02-17 20:02:39 +0300

    Стилизация

--- script.js
+++ script.js
@@ -4,11 +4,47 @@ let butNext = document.querySelector(".next");
 let wVBox = document.getElementsByClassName("slider_box")[0];
 let slide = document.querySelectorAll(".slide"); // записывает как NodeList (arrey)
 let butts = document.querySelector(".buttons");
+let colors = [];
 
 let widthCard = 450; // ширина карточки
 let margin = 20; // отступ от карточки
 let widthSlide = widthCard + margin * 2;
 let countElem = (window.innerWidth <= 1000) ? 1 : 2; // слайдов на view box
+
+
+
+let sLine = 0;
+/* **************************************************************** */
+
+/* ------------------- RANDOM BACKGROUND-COLOR -------------------- */
+
+
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+function randomColor(min, max) {
+    return "#"+
+    getRandomIntInclusive(min, max).toString(16)+
+    getRandomIntInclusive(min, max).toString(16)+
+    getRandomIntInclusive(min, max).toString(16)+
+    getRandomIntInclusive(min, max).toString(16)+
+    getRandomIntInclusive(min, max).toString(16)+
+    getRandomIntInclusive(min, max).toString(16);
+}
+
+for (let i = 0; i < slide.length; i++) {
+    colors.push( randomColor(2, 10) );
+}
+
+for (let i = 0; i < slide.length; i++) {
+    slide[i].style.backgroundColor = colors[i];
+}
+
+/* **************************************************************** */
 
 if (window.innerWidth <= 1000) {
     widthSlide = window.innerWidth;
@@ -24,14 +60,8 @@ if (window.innerWidth <= 1000) {
     true;
 }
 
-let maxSLine = (line[0].childElementCount - countElem) * widthSlide;
-let sLine = 0;
-
-
-
 wVBox.style.width = (widthSlide * countElem) + "px"; // width view box slider
-
-
+let maxSLine = (line[0].childElementCount - countElem) * widthSlide; // Максимальная длинна slider_line
 
 function nextAct() {
     if (sLine == maxSLine) {
@@ -60,8 +90,9 @@ function prevAct() {
 
 }
 
+/* *************************************************************** */
 
-
+// --------------------EVENT ZONE------------------------------------
 
 butPrev.addEventListener("mousedown", () => { prevAct() });
 butNext.addEventListener("mousedown", () => { nextAct() });
