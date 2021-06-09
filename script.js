6ce996b5a51bf586a275ff91931111dee0aa8921
alesiobb <loja.bobaev2000@yandex.ru>
2021-06-09 19:15:05 +0300

    formatting js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 let itemUp = document.querySelectorAll(".item");
 let itemUnder = document.querySelectorAll(".item_under");
 let butt = document.querySelectorAll(".buttOpen");
-let arrIFor =[];
+let arrIFor = [];
 function open(a) {
-    if (arrIFor[a] == 0) {
-        butt[a].style.transform = "rotate(270deg)";
-        itemUnder[a].style.display = "block";
-        arrIFor[a]++;
-    }else {
-        butt[a].style.transform = "rotate(90deg)";
-        itemUnder[a].style.display = "none";
-        arrIFor[a]--;
-    }
+  if (arrIFor[a] == 0) {
+    butt[a].style.transform = "rotate(270deg)";
+    itemUnder[a].style.display = "block";
+    arrIFor[a]++;
+  } else {
+    butt[a].style.transform = "rotate(90deg)";
+    itemUnder[a].style.display = "none";
+    arrIFor[a]--;
+  }
 }
 
 for (let i = 0; i < itemUp.length; i++) {
-    arrIFor.push(0);
-    itemUp[i].addEventListener("mouseup", ()=>{open(i)});
+  arrIFor.push(0);
+  itemUp[i].addEventListener("mouseup", () => {
+    open(i);
+  });
 }
