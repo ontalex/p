283062cda4d50c37d54af7c88949f0628567549d
alesiobb <loja.bobaev2000@yandex.ru>
2021-06-09 18:58:21 +0300

    formatting js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let
     itemUp = document.querySelectorAll(".item"),
     itemUnder = document.querySelectorAll(".item_under"),
-    arrIFor =[],
     butt = document.querySelectorAll(".buttOpen"),
-    open = function(a) {
+    arrIFor =[],
+    function open(a) {
         if (arrIFor[a] == 0) {
             butt[a].style.transform = "rotate(270deg)";
             itemUnder[a].style.display = "block";
@@ -19,4 +19,3 @@ for (let i = 0; i < itemUp.length; i++) {
     arrIFor.push(0);
     itemUp[i].addEventListener("mouseup", ()=>{open(i)});
 }
-
