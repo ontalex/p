96a366427c4060dce80ed8eb15e8a2b62ad4cf65
alesiobb <loja.bobaev2000@yandex.ru>
2021-06-21 20:39:13 +0300

    add new!

--- test/script.js
+++ test/script.js
@@ -1,4 +1,10 @@
-console.log("----test zone----");
+let box = document.querySelector(".slider");
+let items = document.getElementsByClassName("item");
 
-//    Переписать и изучить цепочку промисов
+function prev() {
+    items[0].before(items[items.length - 1]);
+}
 
+function next() {
+    items[items.length - 1].after(items[0]);
+}
