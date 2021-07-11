cc78cb98ae2ec6ef8b9a3c1856e784a3dfb5cae4
alesiobb <loja.bobaev2000@yandex.ru>
2021-07-12 00:42:05 +0300

    Mode test-page

--- test/script.js
+++ test/script.js
@@ -1,10 +1,43 @@
-let box = document.querySelector(".slider");
-let items = document.getElementsByClassName("item");
+// Задание: Калькулятор чисел
 
-function prev() {
-    items[0].before(items[items.length - 1]);
+// Получаем все динамические элемнты
+let 
+  numsBox = document.querySelector(".nums"),
+  out = document.getElementsByClassName("out")[0],
+  oper = {
+    plus : document.querySelector(".plus"),
+    minus: document.querySelector(".minus"),
+    get: document.querySelector(".get")
+  };
+
+function g() {
+  console.log("Good");
 }
 
-function next() {
-    items[items.length - 1].after(items[0]);
+// Слушатели
+
+numsBox.onclick = function(event) { // Ввод чисел
+  if(event.target.className == numsBox.firstElementChild.className) {
+    let target = event.target;
+  }
+
 }
+
+plus.addEventListener("click", operPlus);
+minus.addEventListener("click", operMinus);
+get.addEventListener("click", operGet);
+
+
+// Функции
+
+function operPlus() {
+  
+}
+function operMinus() {
+
+}
+function operGet() {
+
+}
+
+
