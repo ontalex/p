061804ecc1b52e18b28f189d0ef1da03f04b0a10
Your Name <loja.bobae2000@yandex.ru>
2022-02-12 13:08:30 +0300

    edit and degabing elemens

--- layouts/layout_8/main.js
+++ layouts/layout_8/main.js
@@ -1,2 +1,19 @@
-let body = document.querySelector("body");
-body.style.minHeight = window.outerHeight - 90 + "px";
+let password = document.querySelector(".password");
+let alertIsCopy = document.querySelector(".alert");
+
+function showing() {
+  alertIsCopy.classList.remove("hidden-alert");
+  alertIsCopy.classList.add("shown-alert");
+
+  setTimeout(() => {
+    alertIsCopy.classList.remove("shown-alert");
+    alertIsCopy.classList.add("hidden-alert");
+  }, 2000);
+}
+
+password.addEventListener("click", (e) => {
+  showing();
+  let elem = e.target;
+  console.log(elem.innerText);
+  navigator.clipboard.writeText(elem.innerText);
+});
