401e6d37b132d80da65bb9bcb0ed7d06a05a8d7b
Your Name <loja.bobae2000@yandex.ru>
2022-07-17 00:33:51 +0300

    Full настройка меню + адаптив

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let button_menu = document.querySelector(".header_button");
+let float_menu = document.querySelector('.header_menu--float');
+
 let isOpenMenu = false;
 
 function close_openMenu() {
@@ -10,6 +12,8 @@ function close_openMenu() {
 
     console.log(`Статус меню: ${isOpenMenu}`);
 
+    float_menu.classList.remove("header_menu--float-closed");
+    float_menu.classList.add("header_menu--float-opened");
   } else {
     button_menu.classList.remove("header_button--close");
     button_menu.classList.add("header_button--open");
@@ -17,7 +21,18 @@ function close_openMenu() {
     isOpenMenu = false;
 
     console.log(`Статус меню: ${isOpenMenu}`);
+
+    float_menu.classList.remove("header_menu--float-opened");
+    float_menu.classList.add("header_menu--float-closed");
+
+  }
+}
+function fixing_floatMenu() {
+  if(window.scrollY >= document.querySelector("header").offsetHeight) {
+    document.querySelector("header").style.position = "fixed";
+    document.querySelector(".baner").style.paddingTop = document.querySelector("header").offsetHeight+"px";
   }
 }
 
 button_menu.addEventListener("mouseup", close_openMenu);
+window.addEventListener("scroll", fixing_floatMenu);
