799ee0438c0a3d9e417b8070ac29b4af8f3f4151
Your Name <loja.bobae2000@yandex.ru>
2022-07-17 11:28:22 +0300

    ДОбавлена анимация написания кода

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
 let button_menu = document.querySelector(".header_button");
-let float_menu = document.querySelector('.header_menu--float');
+let float_menu = document.querySelector(".header_menu--float");
 
 let isOpenMenu = false;
+
+function addEffectWriting(element, time) {
+  console.log(element);
+
+  let element_array = element.innerText.split("");
+
+  console.log(element_array);
+
+  element.count = 0;
+  element.innerText = "";
+
+  function writeSymbols() {
+    element.innerText += element_array[element.count];
+    element.count++;
+
+    console.log(element.count);
+
+    if (element.count <= element_array.length) {
+      setTimeout(() => {
+        writeSymbols();
+      }, time);
+    } else if (element.count >= element_array.length) {
+      element.count = 0;
+      element.innerText = "";
+
+      writeSymbols();
+    }
+  }
+  writeSymbols();
+}
 
 function close_openMenu() {
   if (!isOpenMenu) {
@@ -24,15 +54,17 @@ function close_openMenu() {
 
     float_menu.classList.remove("header_menu--float-opened");
     float_menu.classList.add("header_menu--float-closed");
-
   }
 }
 function fixing_floatMenu() {
-  if(window.scrollY >= document.querySelector("header").offsetHeight) {
+  if (window.scrollY >= document.querySelector("header").offsetHeight) {
     document.querySelector("header").style.position = "fixed";
-    document.querySelector(".baner").style.paddingTop = document.querySelector("header").offsetHeight+"px";
+    document.querySelector(".baner").style.paddingTop =
+      document.querySelector("header").offsetHeight + "px";
   }
 }
 
 button_menu.addEventListener("mouseup", close_openMenu);
 window.addEventListener("scroll", fixing_floatMenu);
+
+addEffectWriting(document.querySelector('.baner_language-js'), 15);
