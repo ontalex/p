30251151d5295e269e48679e2a3cd6bcd858cd9f
Your Name <loja.bobae2000@yandex.ru>
2022-08-07 15:37:14 +0300

    Чистка

--- layouts/layout_4/main.js
+++
@@ -1,7 +0,0 @@
-console.log('start');
-
-let header = document.querySelector("header");
-let beginBox = document.querySelectorAll(".beginBox");
-let nav = document.querySelector("nav");
-header.style.height = window.innerHeight + "px";
-beginBox[0].style.height = window.innerHeight - nav.clientHeight + "px";
