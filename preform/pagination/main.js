31abee99f8ac8acbe63bd1a3a66e493785d13a78
Your Name <loja.bobae2000@yandex.ru>
2022-08-08 22:49:28 +0300

    Preform +Pagination

---
+++ preform/pagination/main.js
@@ -0,0 +1,52 @@
+let articles = document.querySelector('.articles');
+let pages = document.querySelector('.pages');
+
+function loadArticles(startItem, countItems) {
+    const requestURl = `https://jsonplaceholder.typicode.com/posts?_start=${startItem}&_limit=${countItems}`;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', requestURl)
+
+    xhr.responseType = 'json'
+
+    xhr.onload = () => {
+        var base = xhr.response;
+        articles.innerHTML = "";
+        base.forEach(article => {
+            articles.innerHTML +=
+                `<article class="article">
+                <p class="article_id">article id: <span>${article.id}</span></p>
+                <h2 class="article_header">${article.title}</h2>
+                <p class="article_text">${article.body}</p>
+                </article>`;
+        });
+        pages.addEventListener("click", (e)=changePage);
+    }
+
+    xhr.send();
+}
+
+loadButtons();
+
+function loadButtons() {
+    let page = 1;
+    let maxPage = 5
+
+    for(let i = page; i<=maxPage; i++) {
+        pages.innerHTML += `<a href="#"><button topage="${i}" class="toPage"><span>${i}</span></button></a>`;
+    }
+}
+
+function changePage(event) {
+    console.log(event);
+    console.log(event.target);
+
+    let begin = +event.target.innerText * 5 - 5;
+
+    loadArticles(begin, 5);
+
+    pages.removeEventListener("click", (e)=changePage);
+}
+
+loadArticles(0, 5)
