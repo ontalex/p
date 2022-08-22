5ee10eb4950ccf4059fb4f297e252a121baf80ca
Your Name <loja.bobae2000@yandex.ru>
2022-08-22 20:55:49 +0300

    Добавленны боковые огранечители (touchSlider (preform))

--- preform/touchSlider/main.js
+++ preform/touchSlider/main.js
@@ -10,6 +10,8 @@ let state = {
 
 function touchstart(event) {
     state.start = event.changedTouches[0].clientX;
+
+
     document.querySelector(".cl_start").innerText = state.start;
 }
 
@@ -19,8 +21,16 @@ function touchmove(event) {
     state.path = 1 * (event.changedTouches[0].clientX - state.start);
 
     state.move = state.end + state.path*1.5;
+    if(state.move >= 50) {
+        state.move = 50;
+    }
+    if(state.move <=  -(slider_line.clientWidth - slider_view.clientWidth)) {
+        state.move = -(slider_line.clientWidth - slider_view.clientWidth);
+    }
 
     slider_line.style.left = state.move + "px";
+
+    
 
     document.querySelector('.cl_left').innerText = state.move;
     document.querySelector(".cl_move").innerText = state.path;
