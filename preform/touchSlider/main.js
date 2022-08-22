1f28b34b5c034c5a2de9fcf303acac84d772bcf7
Your Name <loja.bobae2000@yandex.ru>
2022-08-22 21:01:49 +0300

    Чистка

--- preform/touchSlider/main.js
+++ preform/touchSlider/main.js
@@ -1,47 +1,29 @@
 let slider_view = document.querySelector('.slider_view');
 let slider_line = document.querySelector('.slider_line');
-
-let state = {
+let state       = {
     start: 0,
-    end: 0,
-    move: 0,
-    path: 0,
+    end  : 0,
+    move : 0,
+    path : 0,
 }
-
 function touchstart(event) {
     state.start = event.changedTouches[0].clientX;
-
-
-    document.querySelector(".cl_start").innerText = state.start;
 }
-
-
-
 function touchmove(event) {
     state.path = 1 * (event.changedTouches[0].clientX - state.start);
 
-    state.move = state.end + state.path*1.5;
-    if(state.move >= 50) {
+    state.move = state.end + state.path * 1.5;
+    if (state.move >= 50) {
         state.move = 50;
     }
-    if(state.move <=  -(slider_line.clientWidth - slider_view.clientWidth)) {
+    if (state.move <= -(slider_line.clientWidth - slider_view.clientWidth)) {
         state.move = -(slider_line.clientWidth - slider_view.clientWidth);
     }
 
     slider_line.style.left = state.move + "px";
-
-    
-
-    document.querySelector('.cl_left').innerText = state.move;
-    document.querySelector(".cl_move").innerText = state.path;
 }
-
-
-
 function touchend(event) {
     state.end = state.move;
-
-    document.querySelector(".cl_stop").innerText = state.end;
 }
 
 slider_view.addEventListener("touchstart", touchstart, false);
