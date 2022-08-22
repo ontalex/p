08125ba473fa7c5edb4e4fa7dca567a88cbc27c9
Your Name <loja.bobae2000@yandex.ru>
2022-08-22 13:10:26 +0300

    add touchSlider (preform)

---
+++ preform/touchSlider/main.js
@@ -0,0 +1,39 @@
+let slider_view = document.querySelector('.slider_view');
+let slider_line = document.querySelector('.slider_line');
+
+let state = {
+    start: 0,
+    end: 0,
+    move: 0,
+    path: 0,
+}
+
+function touchstart(event) {
+    state.start = event.changedTouches[0].clientX;
+    document.querySelector(".cl_start").innerText = state.start;
+}
+
+
+
+function touchmove(event) {
+    state.path = 1 * (event.changedTouches[0].clientX - state.start);
+
+    state.move = state.end + state.path*1.5;
+
+    slider_line.style.left = state.move + "px";
+
+    document.querySelector('.cl_left').innerText = state.move;
+    document.querySelector(".cl_move").innerText = state.path;
+}
+
+
+
+function touchend(event) {
+    state.end = state.move;
+
+    document.querySelector(".cl_stop").innerText = state.end;
+}
+
+slider_view.addEventListener("touchstart", touchstart, false);
+slider_view.addEventListener("touchmove", touchmove, false);
+slider_view.addEventListener("touchend", touchend, false);
