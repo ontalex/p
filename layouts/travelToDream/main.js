1ce8ba7f9a4abc556c623165dea3c351a6f93ecb
Your Name <loja.bobae2000@yandex.ru>
2022-08-23 22:47:31 +0300

    Добавлено выподающее меню

--- layouts/travelToDream/main.js
+++ layouts/travelToDream/main.js
@@ -1,13 +1,15 @@
-let button_prev = document.querySelector('.button_prev');
-let button_next = document.querySelector('.button_next');
-let slider_line = document.querySelector('.slider_line');
-let slider_index = 1;
-let slide_gap = 20;
-let slide_border = 3;
+let button_prev      = document.querySelector('.button_prev');
+let button_next      = document.querySelector('.button_next');
+let slider_line      = document.querySelector('.slider_line');
+let free_menu        = document.querySelector('.free_menu');
+let button_menu      = document.querySelector('.header_btn--menu');
+let slider_index     = 1;
+let slide_gap        = 20;
+let slide_border     = 3;
 let last_left_margin = 0;
-let count_view = 3;
+let count_view       = 3;
 
-function slider_next(e) {
+function next_scroll_slider(e) {
     console.log(slider_index);
     if (slider_index == slider_line.childElementCount - count_view) {
         button_next.classList.add("inactive_button")
@@ -18,14 +20,13 @@ function slider_next(e) {
         if (slider_index == 1) {
             button_prev.classList.remove("inactive_button")
         }
-        last_left_margin -= document.querySelector('.slide').clientWidth + slide_gap + slide_border * 2;
-        slider_line.style.left = last_left_margin + "px";
+        last_left_margin       -= document.querySelector('.slide').clientWidth + slide_gap + slide_border * 2;
+        slider_line.style.left  = last_left_margin + "px";
         slider_index++;
     }
 
 }
-
-function slider_prev(e) {
+function prev_scroll_slider(e) {
     console.log("hi 2");
     button_next.classList.remove("inactive_button");
     if (slider_index == 2) {
@@ -34,19 +35,19 @@ function slider_prev(e) {
     if (slider_index == 1) {
         return true;
     } else {
-        last_left_margin += document.querySelector('.slide').clientWidth + slide_gap + slide_border * 2;
-        slider_line.style.left = last_left_margin + "px";
+        last_left_margin       += document.querySelector('.slide').clientWidth + slide_gap + slide_border * 2;
+        slider_line.style.left  = last_left_margin + "px";
         slider_index--;
     }
 }
 function touchSlider() {
     let slider_view = document.querySelector('.slider_view');
     let slider_line = document.querySelector('.slider_line');
-    let state = {
+    let state       = {
         start: 0,
-        end: 0,
-        move: 0,
-        path: 0,
+        end  : 0,
+        move : 0,
+        path : 0,
     }
     function touchstart(event) {
         state.start = event.changedTouches[0].clientX;
@@ -74,8 +75,18 @@ function touchSlider() {
     slider_view.addEventListener("touchmove", touchmove, false);
     slider_view.addEventListener("touchend", touchend, false);
 }
+function click_btn_menu() {
+    if (free_menu.classList == "free_menu free_menu--closed") {
+        free_menu.classList.remove("free_menu--closed");
+        free_menu.classList.add("free_menu--opened");
+    } else if (free_menu.classList == "free_menu free_menu--opened") {
+        free_menu.classList.remove("free_menu--opened");
+        free_menu.classList.add("free_menu--closed");
+    }
+}
 
-button_next.addEventListener('mouseup', slider_next);
-button_prev.addEventListener('mouseup', slider_prev);
+button_next.addEventListener('mouseup', next_scroll_slider);
+button_prev.addEventListener('mouseup', prev_scroll_slider);
+button_menu.addEventListener('click', click_btn_menu);
 
 touchSlider();
