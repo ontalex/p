17efc891359e1a122ba7ba8e2306d7c34992d6a7
Your Name <loja.bobae2000@yandex.ru>
2022-10-23 19:24:17 +0300

    Исправление перехода с блока на блок

--- layouts/GenshinImpactBot/main.js
+++ layouts/GenshinImpactBot/main.js
@@ -4,22 +4,12 @@ const description_text_buy = document.querySelector('.description_text_buy');
 const header_link = document.querySelector('.header_link');
 const header_info = document.querySelector('.header_info');
 
-const shift_about_buy = function () {
-    if (!description_text_about.classList.contains("description_text--hidden")) {
-        description_text_about.classList.add("description_text--hidden");
-        description_text_buy.classList.toggle("description_text--hidden");
-    } else {
-        description_text_buy.classList.add("description_text--hidden");
-        description_text_about.classList.toggle("description_text--hidden");
-    }
-}
-
 header_link.addEventListener("click", () => {
-    console.log("Click 1");
-    shift_about_buy();
+    description_text_about.classList.add("description_text--hidden");
+    description_text_buy.classList.remove("description_text--hidden");
 });
 
 header_info.addEventListener("click", () => {
-    console.log("Click 2");
-    shift_about_buy();
+    description_text_buy.classList.add("description_text--hidden");
+    description_text_about.classList.remove("description_text--hidden");
 });
