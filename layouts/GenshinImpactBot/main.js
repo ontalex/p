94e8bb7fa3981d85a7c3b35fa20ddf31127ea5dc
Your Name <loja.bobae2000@yandex.ru>
2022-10-23 16:54:58 +0300

    Работает смена разделов

---
+++ layouts/GenshinImpactBot/main.js
@@ -0,0 +1,25 @@
+const description_text_about = document.querySelector('.description_text_about');
+const description_text_buy = document.querySelector('.description_text_buy');
+
+const heaedr_link = document.querySelector('.header_link');
+const header_info = document.querySelector('.header_info');
+
+const shift_about_buy = function () {
+    if (!description_text_about.classList.contains("description_text--hidden")) {
+        description_text_about.classList.add("description_text--hidden");
+        description_text_buy.classList.toggle("description_text--hidden");
+    } else {
+        description_text_buy.classList.add("description_text--hidden");
+        description_text_about.classList.toggle("description_text--hidden");
+    }
+}
+
+heaedr_link.addEventListener("click", () => {
+    console.log("Click 1");
+    shift_about_buy();
+});
+
+header_info.addEventListener("click", () => {
+    console.log("Click 2");
+    shift_about_buy();
+});
