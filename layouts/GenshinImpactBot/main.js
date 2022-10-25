bf6b3415deec3a04a08a10ff782978a690cb8ad0
Your Name <loja.bobae2000@yandex.ru>
2022-10-25 20:18:55 +0300

    Коректирование макета отностильно ТЗ и исходных скетчей

--- layouts/GenshinImpactBot/main.js
+++ layouts/GenshinImpactBot/main.js
@@ -5,11 +5,17 @@ const header_link = document.querySelector('.header_link');
 const header_info = document.querySelector('.header_info');
 
 header_link.addEventListener("click", () => {
+    header_link.classList.toggle("header_button--hidden");
+    header_info.classList.toggle("header_button--hidden");
+
     description_text_about.classList.add("description_text--hidden");
     description_text_buy.classList.remove("description_text--hidden");
 });
 
 header_info.addEventListener("click", () => {
+    header_info.classList.toggle("header_button--hidden");
+    header_link.classList.toggle("header_button--hidden");
+
     description_text_buy.classList.add("description_text--hidden");
     description_text_about.classList.remove("description_text--hidden");
 });
